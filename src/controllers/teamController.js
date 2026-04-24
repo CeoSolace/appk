f0e79a534a52b3a4d1a0d6d77419fe524e5a3f4d
solace <monkdev9@gmail.com>
2026-04-24 16:38:56 +0100

    Update teamController.js

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -1,22 +1,24 @@
 const Team = require('../models/Team');
+const ScrimPost = require('../models/ScrimPost');
+const User = require('../models/User');
 
 exports.viewTeams = async (req, res, next) => {
   try {
     const teams = await Team.find().sort({ createdAt: -1 });
 
-    res.render('teams/index', {
+    return res.render('teams/index', {
       title: 'Teams',
       teams,
       errors: [],
     });
   } catch (err) {
-    next(err);
+    return next(err);
   }
 };
 
 exports.viewTeam = async (req, res, next) => {
   try {
-    const team = await Team.findOne({ slug: req.params.slug });
+    const team = await Team.findOne({ slug: req.params.slug }).lean();
 
     if (!team) {
       return res.status(404).render('errors/404', {
@@ -24,12 +26,37 @@ exports.viewTeam = async (req, res, next) => {
       });
     }
 
-    res.render('teams/show', {
+    const scrims = await ScrimPost.find({
+      team: team._id,
+      archived: false,
+    })
+      .sort({ createdAt: -1 })
+      .lean();
+
+    let members = [];
+
+    if (team.members && team.members.length) {
+      const users = await User.find({
+        _id: { $in: team.members },
+      })
+        .select('username')
+        .lean();
+
+      members = users.map((user) => ({
+        user,
+        role: String(user._id) === String(team.creator) ? 'Owner' : 'Member',
+      }));
+    }
+
+    return res.render('teams/show', {
       title: team.name,
       team,
+      members,
+      scrims,
       errors: [],
     });
   } catch (err) {
-    next(err);
+    console.error('View team error:', err);
+    return next(err);
   }
 };
